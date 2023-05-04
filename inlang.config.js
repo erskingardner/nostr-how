5f0cdcaf68a647edea2116c32164752e2bfe535f
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-05-04 10:49:08 +0200

    Update inlang config and add smooth-scoll

--- inlang.config.js
+++ inlang.config.js
@@ -1,14 +1,17 @@
 export async function defineConfig(env) {
-	const { default: pluginJson } = await env.$import(
-		'https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@2/dist/index.js'
-	);
+    const { default: pluginJson } = await env.$import(
+        'https://cdn.jsdelivr.net/npm/@erskingardner/inlang-plugin-json@latest/dist/index.js'
+    );
 
-	const { default: standardLintRules } = await env.$import(
-		'https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@2/dist/index.js'
-	);
+    const { default: standardLintRules } = await env.$import(
+        'https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@2/dist/index.js'
+    );
 
-	return {
-		referenceLanguage: 'en',
-		plugins: [pluginJson({ pathPattern: './src/lib/locales/{language}.json' }), standardLintRules()]
-	};
+    return {
+        referenceLanguage: 'en',
+        plugins: [
+            pluginJson({ pathPattern: './src/lib/locales/{language}.json' }),
+            standardLintRules()
+        ]
+    };
 }
