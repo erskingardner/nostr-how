b3e88eb10413f8f911ba8fe28cf35a8cd853f256
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-07-04 09:40:42 -0400

    Enable persian in the language switcher (#103)

--- src/lib/config/l10n.ts
+++ src/lib/config/l10n.ts
@@ -8,8 +8,8 @@ export const locales: localesType[] = [
     { alpha2Code: 'en', name: 'English' },
     { alpha2Code: 'es', name: 'Español' },
     { alpha2Code: 'fr', name: 'Français' },
-    { alpha2Code: 'it', name: 'Italiano' }
-    // { alpha2Code: 'fa', name: 'فارسی' }
+    { alpha2Code: 'it', name: 'Italiano' },
+    { alpha2Code: 'fa', name: 'فارسی' }
 ];
 
 // Returns an array of the supported alpha2 codes
