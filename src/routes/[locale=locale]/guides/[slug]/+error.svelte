0a13b7b636f29af10fec33a5be12de2d2282f9ad
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2024-03-22 11:29:43 +0100

    Update urls for error pages

--- src/routes/[locale=locale]/guides/[slug]/+error.svelte
+++ src/routes/[locale=locale]/guides/[slug]/+error.svelte
@@ -1,16 +1,13 @@
 <script>
-    import { page } from '$app/stores';
-    import PageHeader from '$lib/components/PageHeader.svelte';
-    import { _, isLoading } from 'svelte-i18n';
+    import { page } from "$app/stores";
+    import PageHeader from "$lib/components/PageHeader.svelte";
+    import { _, isLoading } from "svelte-i18n";
 </script>
 
 {#if $page.error && !$isLoading}
     <PageHeader text={$page.error.title} />
     <p>{$page.error.message}</p>
     <p>
-        <a
-            href="https://snort.social/p/npub1zuuajd7u3sx8xu92yav9jwxpr839cs0kc3q6t56vd5u9q033xmhsk6c2uc"
-            >{$_('contactMe')}</a
-        >
+        <a href="https://primal.net/jeffg">{$_("contactMe")}</a>
     </p>
 {/if}
