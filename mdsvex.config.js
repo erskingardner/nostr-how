1abf6121a99e45f3e3c537abe2ad7b9befff9b08
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2024-03-22 11:32:20 +0100

    Update dependencies and add rel=follow and target="_blank" to markdown links

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,8 +1,11 @@
+import rehypeExternalLinks from "rehype-external-links";
+
 const config = {
     extensions: [".svx", ".md"],
     smartypants: {
         dashes: "oldschool",
     },
+    rehypePlugins: [[rehypeExternalLinks, { target: "_blank", rel: ["follow"], nofollow: false }]],
 };
 
 export default config;
